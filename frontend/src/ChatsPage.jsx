4fa771e328b5e800eb1b7642b0f1adb210d9e13c
unknown <justinmoranre@gmail.com>
2023-04-28 13:28:15 -0400

    working chats page

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,18 @@
-const ChatsPage = () => {
-    return <>chats...</>
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced'
+
+const ChatsPage = (props) => {
+    const chatProps = useMultiChatLogic(
+        'b961045b-43bf-4a67-ab91-77b81e0c9c1c', 
+        props.user.username, 
+        props.user.secret
+        );
+
+    return (
+        <div style={{ height: '100vh' }}>
+            <MultiChatSocket {...chatProps} />
+            <MultiChatWindow {...chatProps} style={{ height: '100%' }} />
+        </div>
+    )
 }
 
 export default ChatsPage
